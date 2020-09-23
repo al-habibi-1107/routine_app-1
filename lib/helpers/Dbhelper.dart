ed02bc558da76d45ea4ce8584d56928a2d5713cb
Kamil <kamilanwar2001@gmail.com>
2020-09-23 15:00:38 +0400

    Db -1

---
+++ lib/helpers/Dbhelper.dart
@@ -0,0 +1,33 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+
+class DBHelper {
+
+// Function to access the database
+  static Future<sql.Database> database() async {
+// get a path to the database
+    final dbPath = await sql.getDatabasesPath();
+// Try opening the db , if it dosent exist , create a new db with 
+// the following specifications
+    return sql.openDatabase(
+      path.join(dbPath, 'classes.db'),
+      onCreate: (db, version) async {
+        db.execute(
+          'CREATE TABLE classes(id TEXT,subject TEXT,teacher TEXT,icon TEXT,color TEXT,startTime TEXT,endTime TEXT,day TEXT)'
+        );
+      },
+    );
+  }
+
+  Future<void> createDB(String table,Map<String,Object> data)async{
+    final db = await database();
+    db.insert(table, data,conflictAlgorithm:sql.ConflictAlgorithm.replace);
+
+
+  }
+
+
+
+
+}
